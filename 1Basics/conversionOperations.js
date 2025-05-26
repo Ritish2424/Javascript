ff07f3696d582522d459b3b6f59141575e0a1586
Ritish2424 <dankritish69@gmail.com>
2025-05-26 09:50:54 +0000

    conversionOperations

---
+++ 1Basics/conversionOperations.js
@@ -0,0 +1,30 @@
+// let score="33"
+// let score="33abc"
+// let score=null
+let score =true
+
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber= Number (score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33"= 33
+// "33abc"=Nan
+//tru=1
+//false=0
+
+
+// let isLoggedIn="ritish"
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+let someNumber=33
+let StringNumber= String(someNumber)
+console.log(StringNumber)
+console.log(typeof StringNumber)
+
+
