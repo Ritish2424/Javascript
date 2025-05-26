c6981d6ffb8c0b97eb85f8f2ba4f38fee4aa3176
Ritish2424 <dankritish69@gmail.com>
2025-05-26 09:21:34 +0000

    Datatypes

---
+++ 1Basics/datatypes.js
@@ -0,0 +1,36 @@
+"use Strict";
+// alert(3+3) we are using node js not broser
+
+
+console.log(3+3);
+console.log("Ritish");
+// code readibility should be high
+
+
+let name="Ritish"  
+let age=21
+let isLoggedIn= false
+
+
+// number=>2 to power 53
+//bigint
+//strin=>""
+//boolean =true of false
+//null=>standalone value(empty)
+// undefined=>variable declare but value not defined
+//symbol=>uniqueness
+
+
+
+
+
+
+//object
+
+// console.log(typeof"ritish")
+// console.log(typeof age);
+// console.log(typeof null); type is object
+console.log(typeof undefined);
+
+
+
