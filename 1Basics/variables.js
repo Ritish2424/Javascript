132c523b02d6b8718a5c1f4ac7bc5557d8aa717f
Ritish2424 <dankritish69@gmail.com>
2025-05-26 09:21:11 +0000

    variables

---
+++ 1Basics/variables.js
@@ -0,0 +1,13 @@
+const accountId = 14556
+let accountEmail = "Ritishkanojia2424@gmail.uk"
+var accountPassword = "123456"
+accountCity = "Delhi"
+
+// accountId = 2 not allowed (const not change)
+accountEmail = "hd@hd.uk"
+accountPassword = "21121"
+accountCity = "Jaipur"
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword,accountCity ]);
+
+
