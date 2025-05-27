004d94846f6bfa75fb7c51f2440a11e186c88086
Ritish2424 <dankritish69@gmail.com>
2025-05-27 09:32:29 +0000

    datatypesSummary

---
+++ 1Basics/datatypesSummary.js
@@ -0,0 +1,39 @@
+//=================primitive datatpes===============//
+// const score=100;
+// const scorevalue=100.3
+// console.log(typeof scorevalue)
+
+// const loggedIn=false
+// const OutsideTemp=null
+// let userEmail;
+// let userEmail=undefined;
+// console.log(typeof userEmail)
+
+// const id =Symbol('123')
+// const anotherId=Symbol('123')
+// console.log( typeof id===anotherId)
+
+
+// const BigNumber=4654546516519n;
+// console.log (typeof BigNumber)
+
+
+
+
+//=================non primmitive datatype=================//
+// const Heros=["Shaktiman" , "Nagraj", "Doga"]
+// let myObj={
+//     name: "Ritish",
+//     age : 21
+
+// }
+// console.log(typeof myObj)
+
+
+
+const myfunction = function () {
+    console.log("Hello World")
+}
+
+
+// /identify the typeof all dataypes
