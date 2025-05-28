21fc095cd61a704475b3da70c05143e8712c0cc6
Ritish2424 <dankritish69@gmail.com>
2025-05-28 09:51:14 +0000

    String

---
+++ 1Basics/String.js
@@ -0,0 +1,45 @@
+// const name="Ritish";
+// const repocount=35;
+// console.log(name+repocount+"value")
+// console.log(`Hello my name is ${name} and my age is ${repocount}`)
+
+const gameName= new String("Ritishk")
+// differernt way to declare string
+// console.log(gameName[0])
+// console.log(gameName.__proto__);
+
+console.log(gameName.length);
+// console.log(gameName.toUpperCase())
+// console.log(gameName.charAt(5));
+
+// console.log(gameName.indexOf('h'))
+
+// const newString=gameName.substring(0,4)
+// console.log(newString);
+
+
+
+// const anotherString=gameName.slice(-6,4)
+// console.log(anotherString);
+
+
+// const newStringOne="  Ritish";
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+
+
+
+// const url="https://Ritish.uk/%20.kanojia";
+// console.log(url.replace('%20','-'));
+// console.log(url.includes('Ritish'));
+// console.log(url.includes('Radhe'));
+
+
+const another='Ritish-Kano-Fame';
+console.log(another.split('-'));
+
+
+
+
+
