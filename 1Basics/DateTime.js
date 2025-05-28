614b96db00fda298db679d48207f8b0df442494f
Ritish2424 <dankritish69@gmail.com>
2025-05-28 16:35:46 +0000

    datetime

---
+++ 1Basics/DateTime.js
@@ -0,0 +1,58 @@
+let mydate=new Date()
+// console.log(mydate);
+// console.log(mydate.to)
+
+
+
+// console.log(mydate.toDateString());
+// console.log(typeof mydate);
+
+
+
+
+// let mycreatedate=new Date(2023,5,23) [Months Starts from zero]
+// console.log(mycreatedate);
+// console.log(mycreatedate.toDateString());
+
+
+
+// let mycreateddate1=new Date(2023,0,23,5,3)
+// console.log(mycreateddate1.toLocaleString());
+
+// let mycreateddate2=new Date("2023-01-24")
+// console.log(mycreateddate2.toLocaleString());
+
+
+let mycreateddate3=new Date("06-24-2023")
+console.log(mycreateddate3.toLocaleString());
+
+
+
+
+
+
+// let mytimestamp=Date.now()
+// console.log(mytimestamp)
+// console.log(mycreateddate3.getTime())
+
+
+// console.log(Date.now()/1000)
+// console.log(Math.floor(Date.now()/1000))
+
+
+let newdate=new Date()
+console.log(newdate);
+// console.log(newdate.) [see the methods]
+
+// 
+
+
+
+newdate.toLocaleString('default',{
+    weekday:"long"
+})
+
+
+
+
+
