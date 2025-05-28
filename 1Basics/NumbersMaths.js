b5d68eb4548a66b7cbe284d8c264b9a8e8b59199
Ritish2424 <dankritish69@gmail.com>
2025-05-28 09:51:34 +0000

    NumersMaths

---
+++ 1Basics/NumbersMaths.js
@@ -0,0 +1,47 @@
+// const score=400;
+// console.log(score);
+
+// const balance= new Number(100);
+// console.log(balance);
+
+
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+
+
+// const anotherNumber=123.9866;
+// console.log(anotherNumber.toPrecision(3));
+
+// const Hundreds=10000000;
+// console.log(Hundreds.toLocaleString());
+// console.log(Hundreds.toLocaleString('en-IN'));
+
+
+//++++++++++++++++++Maths++++++++++++++++++++++++
+// console.log(Math);           [it is an object]
+// console.log(Math.abs(-4))    [it is use to convert negative number into postive]
+// console.log(Math.round(4.3));[it round off the value]
+// console.log(Math.ceil(4.2)); [it round of top]
+// console.log(Math.floor(4.9));[it round of bottom]
+
+
+// console.log(Math.min(4,3,6,8));
+// console.log(Math.max(4,3,6,8));
+
+
+// console.log(Math.random());
+// console.log(Math.random()*10+1); 
+// or
+// console.log((Math.random()*10)+1);
+// console.log((Math.floor(Math.random()*10)+1));
+
+
+
+const min=10;
+const max=20;
+
+console.log(Math.random()*(max-min+1));
+console.log(Math.floor(Math.random()*(max-min)+1));
+console.log(Math.floor(Math.random()*(max-min+1)+min))
