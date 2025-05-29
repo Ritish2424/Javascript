015f61674e5073592a75ac78ded3ac3a5464dfef
Ritish2424 <dankritish69@gmail.com>
2025-05-29 10:04:22 +0000

    Arrays

---
+++ 2Basics/Arrays.js
@@ -0,0 +1,52 @@
+const myArr=[0,1,2,3,4,5]
+// console.log(myArr[0]);
+
+// const MyHeros=["Shaktiman","Nagraj"]
+// console.log(MyHeros[1])
+// const MyArr2=new Array(1,2,3,4,5);
+
+
+//+++++++Arrays Methods+++++++++//
+// myArr.push(6);
+// console.log(myArr) Add the value is Array
+
+// myArr.pop()
+// console.log(myArr) Remove the last value from Arrays
+
+
+
+
+// myArr.unshift(9)
+// console.log(myArr) insert the value from start
+
+// myArr.shift()
+// console.log(myArr) delete teh value from start
+
+
+
+// console.log(myArr.includes(9));
+// console.log(myArr.includes(3));
+
+
+
+// const newArr=myArr.join();
+// console.log(myArr)
+// console.log(newArr)
+// console.log(typeof newArr)
+
+
+
+
+//slice and splice//
+console.log("A",myArr);
+
+
+
+const myn1=myArr.slice(1,3)
+console.log(myn1);
+
+console.log("B",myArr)
+
+const myn2=myArr.splice(1,3)
+console.log("C",myArr)
+console.log(myn2)
