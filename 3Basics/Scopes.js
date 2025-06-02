c57f514f74087a6bdd7cd9a14eacdc44134a0422
Ritish2424 <dankritish69@gmail.com>
2025-06-02 10:11:29 +0000

    Scopes

---
+++ 3Basics/Scopes.js
@@ -0,0 +1,16 @@
+let a=300
+if (true)
+{
+let a = 10
+const b = 20
+console.log("inner",a)
+}
+
+for (leti = 0;i < array.length;i++) {
+    const element = array[i];
+    
+}
+
+console.log(a);
+// console.log(b);
+// console.log(c);
