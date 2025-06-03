8c726739f785b3a6e860647c7d2749a3e980bf92
Ritish2424 <dankritish69@gmail.com>
2025-06-03 09:08:55 +0000

    Scopes

--- 3Basics/Scopes.js
+++ 3Basics/Scopes.js
@@ -3,14 +3,51 @@ if (true)
 {
 let a = 10
 const b = 20
-console.log("inner",a)
+// console.log("inner",a)
 }
 
-for (leti = 0;i < array.length;i++) {
-    const element = array[i];
+// for (leti = 0;i < array.length;i++) {
+//     const element = array[i];
     
-}
 
-console.log(a);
+
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+
+
+
+function one(){
+    const username="ritish"
+    function two(){
+        const website="Youtube"
+        console.log(username)
+    }
+    // console.log(website)
+    two()
+}
+// one()
+
+
+if(true){
+    const username="Ritish"
+    if(username==="Ritish"){
+        const website=" youtube"
+        console.log(username+website)
+    }
+    // console.log(website)
+    console.log(username)
+    }
+    
+
+console.log(adddone(5))
+function adddone(num){
+    return num+1
+}
+
+ const addtwo=function adddone(num){
+    return num+2
+}
+console.log(addtwo(5))
