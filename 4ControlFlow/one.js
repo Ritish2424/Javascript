6024a3a829773bba7664e2a9bad4b5b75138a1c4
Ritish2424 <dankritish69@gmail.com>
2025-06-06 10:34:09 +0000

    IfElse

---
+++ 4ControlFlow/one.js
@@ -0,0 +1,80 @@
+//if statement
+
+
+// const isUserLoggedIn = true
+
+
+// const temprature=41
+
+// if(temprature===40){
+//     console.log("less than 50")
+// }
+// else{
+// console.log("tempreture is greater than 50")
+// }
+
+
+
+// if (2!=3) {
+// console.log("executed")
+
+// }
+
+// <,>,(less than greater than)
+//<= example 2<=2
+
+
+
+// const score=200
+// if(score>100){
+//     var power="fly"
+//     console.log(`user power: ${power}`)
+// }
+//     console.log(`user power: ${power}`)
+// var scope was compltely global
+// use let and const
+
+
+
+// const balance=1000
+// if(balance>500) console.log("test");
+// implicit scope but remmber in same line
+// you can use ,comma if you want to use in different line
+//     const balance=1000
+// if(balance>500) console.log("test"),
+//  console.log("test")
+
+
+// const balance = 1000
+
+// if (balance < 500) {
+//     console.log("less than 500")
+// }
+// else if(balance<750){
+//     console.log("less than 750")
+// }
+// else if(balance<900){
+//     console.log("less than 900")
+
+// }
+// else{
+//     console.log("less than 1200")
+
+// }
+
+
+
+
+const UserLoggedIn = true
+const debitcard = true
+const loggedinfromgoogle=false
+const loggedinfromemail=true
+
+
+if (UserLoggedIn && debitcard && 2==3) {
+    console.log("allow to buy courses")
+
+}
+if(loggedinfromgoogle || loggedinfromemail){
+    console.log("user logged in")
+}
