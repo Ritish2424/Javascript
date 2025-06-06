24fb5401b3d06653d7d3b3d796ec7cc4506c1ed6
Ritish2424 <dankritish69@gmail.com>
2025-06-06 09:06:54 +0000

    iifi

---
+++ 3Basics/iifi.js
@@ -0,0 +1,14 @@
+// immeditalery invoked function expression
+//named effi
+(function chai(){
+    console.log(`db conneed`)
+})();
+// ()first dunction defination
+// () execution call
+// iifi-to prevent from global scope pollution
+
+
+//unamed iffi
+((name)=>{
+    console.log( `db connceted two ${name}`)
+})('Ritish');
